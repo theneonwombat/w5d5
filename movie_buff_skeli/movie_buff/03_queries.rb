a2f50ae130bb728ce1fab68668b3d9fce17e74e4
Alec Caro <alec.caro@gmail.com>
2021-01-29 15:46:42 -0500

    Started 3

--- movie_buff_skeli/movie_buff/03_queries.rb
+++ movie_buff_skeli/movie_buff/03_queries.rb
@@ -1,7 +1,7 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie.joins(:actors).where('actors.name IN (?)', those_actors).pluck(:id, :title)
 end
 
 def golden_age
